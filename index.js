6a23ee1b08e614fc701e94551e579029e1bc169f
mjherr <kanelapr@gmail.com>
2022-05-30 11:54:15 -0400

    fixed mistakes in move

--- index.js
+++ index.js
@@ -2,49 +2,26 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
-let direction = null;
-let x = 100;
-let y = 250;
 
-setInterval (function () {
-if (direction === 'west') {
-    x = x - 1
-};
-if (direction === 'north') {
-    y = y + 1
-};
-if (direction === 'east') {
-    x = x + 1
-};
-if (direction === 'south') {
-    y = y - 1
-};
-}, 1);
-
-document.addEventListener('keyup', function(e) {
-    direction = null
-});
-
-document.addEventListener('keydown', function (e) {
-    if (e.repeat) return;
-
-    if (e.key === 'ArrowLeft') {
-        direction = 'west'
+function handleDirectionChange(direction){
+    if(direction === null){
+        character.src = `assets/green-character/static.gif`
     }
-    if (e.key === 'ArrowUp') {
-        direction = 'north'
+    if(direction === 'west'){
+        character.src = `assets/green-character/west.gif`
     }
-    if (e.key === 'ArrowRight') {
-        direction = 'east'
+    if(direction === 'north'){
+        character.src = `assets/green-character/north.gif`
     }
-    if (e.key === 'ArrowDown') {
-        direction = 'south'
+    if(direction === 'east'){
+        character.src = `assets/green-character/east.gif`
     }
-})
+    if(direction === 'south'){
+        character.src = `assets/green-character/south.gif`
+    }
+}
 
-character.style.left = x + 'px';
-character.style.bottom = y + 'px';
-move(character).to(100, 250);
+move(character).withArrowKeys(100, 250, handleDirectionChange)
 
 move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
